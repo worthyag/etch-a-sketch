a1f93c43e1da14ec66290d817812723e0b7c658b
Worthy <worthyintech@gmail.com>
2022-10-12 22:17:11 +0100

    The user can now choose a grid size.

--- main.js
+++ main.js
@@ -1,18 +1,48 @@
 const container = document.querySelector("#container");
+const btnContainer = document.querySelector(".for-btn");
 let divs = [];
+let size = 256;
+let col;
 
-for (let i = 0; i < 256; i++) {
-    divs.push(document.createElement("div"));
-}
+let btn = document.createElement("button");
+btn.innerText = "Grid Size";
+btn.classList.add("grid-size-btn");
+btnContainer.appendChild(btn);
 
-for (let i = 0; i < divs.length; i++) {
-    // divs[i].innerText = i+1;
-    divs[i].classList.add("boxes");
-    divs[i].addEventListener("mouseover", changeColour);
-    container.appendChild(divs[i]);
+btn.addEventListener("click", gridSize);
+
+createGrid(size);
+
+function createGrid(length){
+    col = Math.sqrt(length);
+    container.style.gridTemplateColumns = `repeat(${col}, 1fr)`;
+
+    for (let i = 0; i < length; i++) {
+        divs.push(document.createElement("div"));
+    }
+    
+    for (let i = 0; i < length; i++) {
+        divs[i].classList.add("boxes");
+        divs[i].addEventListener("mouseover", changeColour);
+        container.appendChild(divs[i]);
+    }
 }
 
 function changeColour(e) {
     e.target.classList.add("hover");
     console.log(e.target);
+}
+
+function gridSize(e) {
+    console.log(e.target);
+    size = prompt("What size would you like your grid to be?", 16);
+    console.log(size, typeof(size));
+    size = parseInt(size);
+    console.log(size, typeof(size));
+    size *= size
+    console.log(size);
+
+    container.textContent = "";
+
+    createGrid(size);
 }
